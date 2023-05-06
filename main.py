44463efb97b03076bbe8c2c20269fd73dda12f4f
Don Dang <doncdang@hotmail.com>
2023-05-06 13:32:34 -0700

    initial init

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Developer: Don Dang
+Assignment: 05 Simple Time Server
+Date: 5/6/23
+File name: main.py
+Purpose:
+- Implement a simple microservice and deploy it to Heroku
+- The microservice will convert the current human-readable time into Epoch time
+
+Resources:
+1. Epoch & Unix Timestamp Conversion Tools
+- https://www.epochconverter.com/
+
+2. flask
+
+3. Python's time.time()
+- https://docs.python.org/3/library/time.html#time.time
+
+
+
+"""
